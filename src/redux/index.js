99aa55647cd72ebf33e2db9fbd091f256073011e
drugstorewoker <drugstoreworker@gmail.com>
2023-02-26 23:31:36 +0600

    feat: add redux

---
+++ src/redux/index.js
@@ -0,0 +1,14 @@
+import { configureStore } from "@reduxjs/toolkit";
+import  {authSlice} from './authSlice'
+
+
+
+const store = configureStore({
+    reducer: {
+        auth: authSlice
+    }
+});
+
+console.log(store);
+
+export default store;
