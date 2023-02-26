55b200c4a3313a5aede949ccc9c9442b941a0cd6
drugstorewoker <drugstoreworker@gmail.com>
2023-02-27 00:31:01 +0600

    feat: redux

---
+++ src/redux/index.js
@@ -0,0 +1,12 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { rootSliceReducer } from "./rootSlice";
+
+const reducers = combineReducers({
+    root: rootSliceReducer
+});
+
+export const store = configureStore({
+    reducer: reducers
+});
+
+console.log(store)
